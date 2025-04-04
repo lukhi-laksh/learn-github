2b2fc4df57b14be0c1ff7b6b3577d4fb871d4884
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-04-04 23:32:30 +0530

    Update AppName.jsx

--- 28-34 forms and UseRef/todo/src/components/AppName.jsx
+++ 28-34 forms and UseRef/todo/src/components/AppName.jsx
@@ -1,7 +1,9 @@
 import styles from "./AppName.module.css";
 
 function AppName() {
+  <h2 class="todo">function start</h2>
   return <h1 className={styles.todoHeading}>TODO App</h1>;
+  
 }
 
 export default AppName;
